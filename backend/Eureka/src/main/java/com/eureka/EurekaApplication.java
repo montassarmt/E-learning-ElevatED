2728d13f2ca2c37fec2f0f0c57f71c8d90422894
Zeineb Jmal <zeinebjmal445@gmail.com>
2025-04-12 00:30:51 +0100

    fixing changes

--- backend/Eureka/src/main/java/com/eureka/EurekaApplication.java
+++ backend/Eureka/src/main/java/com/eureka/EurekaApplication.java
@@ -1,15 +1,15 @@
-package com.example.tpeureka;
+package com.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class TpEurekaApplication {
+@EnableEurekaServer
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TpEurekaApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
